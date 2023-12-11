c1d2172c1bc3bc3a728ff0291af329cb47d4adc8
kurbanhusainseliya <kurbanhusain.seliya@qalbit.com>
2023-12-11 13:40:20 +0530

    add file add

---
+++ js/app.js
@@ -0,0 +1,240 @@
+$(document).ready(function () {
+  $(".section2_h1").waypoint(
+    function (direction) {
+      $(".section2_h1").addClass("animate__animated animate__bounceInDown");
+    },
+    {
+      offset: "400px",
+    }
+  );
+
+  $(".section2_text_flex1").waypoint(
+    function (direction) {
+      $(".section2_text_flex1").addClass(
+        " animate__animated animate__fadeInLeft"
+      );
+    },
+    {
+      offset: "400px",
+    }
+  );
+
+  $(".section2_text_flex2").waypoint(
+    function (direction) {
+      $(".section2_text_flex2").addClass(
+        "animate__animated animate__fadeInRight "
+      );
+    },
+    {
+      offset: "500px",
+    }
+  );
+
+  $(".section2_text_flex3").waypoint(
+    function (direction) {
+      $(".section2_text_flex3").addClass(
+        " animate__animated animate__fadeInLeft"
+      );
+    },
+    {
+      offset: "600px",
+    }
+  );
+
+  $(".section2_text_flex4").waypoint(
+    function (direction) {
+      $(".section2_text_flex4").addClass(
+        "animate__animated animate__fadeInRight "
+      );
+    },
+    {
+      offset: "700px",
+    }
+  );
+
+  $(".section2_leptops_photos").waypoint(
+    function (direction) {
+      $(".section2_leptops_photos").addClass(
+        "animate__animated animate__zoomIn"
+      );
+    },
+    {
+      offset: "400px",
+    }
+  );
+
+  $(".section3_img").waypoint(
+    function (direction) {
+      $(".section3_img").addClass("animate__animated animate__zoomIn ");
+    },
+    {
+      offset: "400px",
+    }
+  );
+
+  $(".section3_h1").waypoint(
+    function (direction) {
+      $(".section3_h1").addClass("animate__animated animate__bounceInDown");
+    },
+    {
+      offset: "700px",
+    }
+  );
+
+  $(".section3_p").waypoint(
+    function (direction) {
+      $(".section3_p").addClass("animate__animated animate__bounceInLeft");
+    },
+    {
+      offset: "800px",
+    }
+  );
+
+  $(".section3_btns").waypoint(
+    function (direction) {
+      $(".section3_btns").addClass("animate__animated animate__jello");
+    },
+    {
+      offset: "800px",
+    }
+  );
+
+  $(".section4_img").waypoint(
+    function (direction) {
+      $(".section4_img").addClass("animate__animated animate__flip");
+    },
+    {
+      offset: "400px",
+    }
+  );
+
+  $(".Innovatief").waypoint(
+    function (direction) {
+      $(".Innovatief").addClass("animate__animated animate__lightSpeedInLeft");
+    },
+    {
+      offset: "600px",
+    }
+  );
+
+  $(".Doeltreffend").waypoint(
+    function (direction) {
+      $(".Doeltreffend").addClass("animate__animated animate__zoomInDown");
+    },
+    {
+      offset: "600px",
+    }
+  );
+
+  $(".Transparant").waypoint(
+    function (direction) {
+      $(".Transparant").addClass(
+        "animate__animated animate__lightSpeedInRight"
+      );
+    },
+    {
+      offset: "600px",
+    }
+  );
+
+  $(".section4_p").waypoint(
+    function (direction) {
+      $(".section4_p").addClass("animate__animated animate__slideInUp");
+    },
+    {
+      offset: "600px",
+    }
+  );
+
+  $(".section5_img").waypoint(
+    function (direction) {
+      $(".section5_img").addClass("animate__animated animate__bounceInRight");
+    },
+    {
+      offset: "400px",
+    }
+  );
+
+  $(".section5_p").waypoint(
+    function (direction) {
+      $(".section5_p").addClass("animate__animated animate__bounceInDown");
+    },
+    {
+      offset: "600px",
+    }
+  );
+
+  $(".section5_h1").waypoint(
+    function (direction) {
+      $(".section5_h1").addClass("animate__animated animate__bounceInLeft");
+    },
+    {
+      offset: "600px",
+    }
+  );
+
+  $(".section5_btns").waypoint(
+    function (direction) {
+      $(".section5_btns").addClass("animate__animated animate__bounceInUp");
+    },
+    {
+      offset: "700px",
+    }
+  );
+
+  $(".footer_bloomford").waypoint(
+    function (direction) {
+      $(".footer_bloomford").addClass("animate__animated animate__rollIn");
+    },
+    {
+      offset: "700px",
+    }
+  );
+
+  $(".footer_h1_1").waypoint(
+    function (direction) {
+      $(".footer_h1_1").addClass("animate__animated animate__rollIn");
+    },
+    {
+      offset: "700px",
+    }
+  );
+
+  $(".footer_h1_2").waypoint(
+    function (direction) {
+      $(".footer_h1_2").addClass("animate__animated animate__rollIn");
+    },
+    {
+      offset: "700px",
+    }
+  );
+
+  $(".vlog-bloomford-heading").waypoint(
+    function (direction) {
+      $(".vlog-bloomford-heading").addClass(
+        "animate__animated animate__rollIn"
+      );
+    },
+    {
+      offset: "700px",
+    }
+  );
+
+  $(".footer_policy").waypoint(
+    function (direction) {
+      $(".footer_policy").addClass("animate__animated animate__slideInUp");
+    },
+    {
+      offset: "900px",
+    }
+  );
+
+  $(".footer_policy").waypoint(
+    function (direction) {
+      $(".footer_policy").addClass("animate__animated animate__slideInUp");
+    },
+    {
+      offset: "900px",
+    }
+  );
+});
